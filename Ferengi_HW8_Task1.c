f4e86f6ebfbdb7b6ba909ca2b541a2690d7ab0d9
Tysen Bair <tysenbair@mail.weber.edu>
2017-04-02 00:04:05 +0000

    first

---
+++ Ferengi_HW8_Task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Ferengi_HW8_Task1.c
+ *       	Usage:  ./Ferengi_HW8_Task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/01/2017 11:55:34 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tysen Bair (), tysenbair@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
