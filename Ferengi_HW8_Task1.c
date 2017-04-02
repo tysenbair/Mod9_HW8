8dd92a275a48e2cf663b04fee1ed2b2b2bed0cca
Tysen Bair <tysenbair@mail.weber.edu>
2017-04-02 04:42:24 +0000

    second

--- Ferengi_HW8_Task1.c
+++ Ferengi_HW8_Task1.c
@@ -13,17 +13,45 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include <string.h>
 
 /* Function Prototypes */
+void Usage(void);
+void Help(void);
+void StringIn(void);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
-
+	if(argc != 3)
+	{	
+		Usage();
+	}
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(void)
+{
+	printf("\n");
+	printf("Missing required parameters\n");
+	printf("Usage:  ./Ferengi_HW8_Task1 <str1> <str2>\n");
+	printf("The program will be checking if str2 is part of str1\n");
+	printf("\n");
+	return;
+}
+
+void Help(void)
+{
+	
+	return;
+}
+
+void StringIn(void)
+{
+	
+	return;
+}
 
 
