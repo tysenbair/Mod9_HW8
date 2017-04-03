05905230028bed501c58448bbb72231b6bd98e5b
Tysen Bair <tysenbair@mail.weber.edu>
2017-04-03 02:09:06 +0000

    third

--- Ferengi_HW8_Task1.c
+++ Ferengi_HW8_Task1.c
@@ -18,14 +18,37 @@
 /* Function Prototypes */
 void Usage(void);
 void Help(void);
-void StringIn(void);
+char *StringIn(char *first, char *last);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
-	if(argc != 3)
-	{	
-		Usage();
+	char *begin = 0;
+	do
+	{ 
+	//	if(argv[0] string itoa(int) == '-')
+	//	{
+	//		Help();
+	//		exit(1);
+	//	}		
+		if(argc != 3)
+		{	
+			Usage();
+			exit(1);
+		}
+		 
+		begin = StringIn(argv[1], argv[2]);
+
+		if(begin != NULL)
+			printf("\n %s is inside %s. \n Returned string %s\n", argv[2], argv[1], begin);
+		
+			else printf("\n %s is not found in %s. \n", argv[2], argv[1] );
+
+			break;
+	}
+	while(1);
+	{
+	printf("\n");
 	}
 	return 0;
 }
@@ -44,14 +67,25 @@ void Usage(void)
 
 void Help(void)
 {
-	
+	printf("\n");
+	printf("Help Information\n");
+	printf("Usage:  ./Ferengi_HW8_Task1 <str1> <str2>\n");
+	printf("The program will be checking if str2 is part of str1\n");
+	printf("\n");
 	return;
 }
 
-void StringIn(void)
+char *StringIn(char *first, char *last)
 {
-	
-	return;
+	char* i = (char *) first;
+	int ii = strlen(last);
+		while((i = strchr(i, *last)) != 0 )
+		{	
+			if (strncmp(i, last, ii) == 0 )
+				return i;
+			i++;
+		}
+	return NULL;
 }
 
 
